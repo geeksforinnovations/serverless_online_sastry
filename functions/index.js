db6f6642a49d52af0745252a7d7e47f1cb3d4bbe
Manjunath14 <Manjunath14@users.noreply.github.com>
2019-10-31 17:08:08 +0530

    Added code for Puja creation

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,12 @@ const readAllPujas = require('./pujas/getAll')
 const getPujaById = require('./pujas/getById')
 const createCustomer = require('./transactions/createCustomer')
 const payment = require('./transactions/payment')
-
+const createPuja = require('./pujas/createPuja')
 
 module.exports = {
     readAllPujas: readAllPujas,
     getPujaById: getPujaById,
     createCustomer:createCustomer,
     payment:payment,
+    createPuja:createPuja
 }
