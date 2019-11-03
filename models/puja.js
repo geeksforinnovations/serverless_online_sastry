d5f6df7474117b891dae827569ded1aacbac4c2a
Manjunath <manjunath.cycle@gmailc.om>
2019-11-03 12:43:23 +0530

    Added validations to Puja model

--- models/puja.js
+++ models/puja.js
@@ -1,13 +1,48 @@
 'use strict';
+var validator = require('validator');
+
 module.exports = (sequelize, DataTypes) => {
   const Puja = sequelize.define('Puja', {
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Name can't be empty."
+        },
+        customValidator(value) {
+          if (value.trim().length === 0) {
+            throw new Error("Name can't be empty");
+          }
+        }
+      }
+    },
     description: DataTypes.STRING,
     about: DataTypes.STRING,
-    timeInHrs: DataTypes.FLOAT,
-    requiredThings:DataTypes.JSON
+    timeInHrs: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        isFloat: {
+          msg: 'TimeInHrs should be float'
+        },
+        notNull: {
+          msg: "TimeInHrs can't be empty."
+        }
+      }
+    },
+    requiredThings: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Required things can't be null."
+        }
+      }
+    }
+
   }, {});
-  Puja.associate = function(models) {
+  Puja.associate = function (models) {
     // associations can be defined here
   };
   return Puja;
