8db8fde6640d0f208014555a4df3fce2432100d1
Manjunath <manjunath.cycle@gmailc.om>
2019-12-25 13:08:50 +0530

    insert language details while puja creation.

--- functions/pujas/createPuja.js
+++ functions/pujas/createPuja.js
@@ -1,20 +1,35 @@
 const dbModels = require('../../models')
 const helpers = require("../../utils/helpers");
 
+
 module.exports = async (event, context, callback) => {
   try {
-    //const puja = JSON.parse(event.body);
-    const puja = {name:'type',description:'puja.description',about:'puja.about',
-      timeInHrs:1
-      ,requiredThings:'puja.requiredThings',
-      pujaType:'Online2',cost:12}
-    const createdPuja = await dbModels.Puja
+    const puja = JSON.parse(event.body);
+     let pujaLanguages=[];
+    //  const puja = {id:0,name:'Test',description:'Test',about:'Test',
+    //   timeInHrs:4
+    //   ,requiredThings:'',
+    //   pujaType:'Offline',cost:10000,pujaLanguageIds:[1,2,3]}
+
+    const Languages = dbModels.Puja.hasMany(dbModels.PujaLanguage, { as: 'languages',foreignKey: 'pujaid' });
+    if(puja.pujaLanguageIds != null && Array.isArray(puja.pujaLanguageIds))
+    puja.pujaLanguageIds.map(element => {
+      pujaLanguages.push({languageId:element});  
+  });            
+    const createdPuja =  await dbModels.Puja
       .create({name:puja.name,description:puja.description,about:puja.about,
         timeInHrs:puja.timeInHrs,requiredThings:puja.requiredThings,
-        pujaType:puja.pujaType,cost:puja.cost});
+        pujaType:puja.pujaType,cost:puja.cost,
+        languages:pujaLanguages
+        },
+         {
+          include: [{
+            association: Languages,
+            as: 'languages'
+          }]
+      });     
     return helpers.success({ data:  createdPuja});
-    //callback(null, x)
-  } catch (error) {
+     } catch (error) {
     return helpers.failure({ message: error.message });
   }
 };
