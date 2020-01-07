e613a51c0b9d6fda5caa5c5eefad6acc7e53e147
Manikumar K V <mkanamarlapudi@nasinsurance.com>
2020-01-07 23:48:26 +0530

    added language in getAll

--- functions/pujas/getAll.js
+++ functions/pujas/getAll.js
@@ -4,7 +4,22 @@ const helpers = require("../../utils/helpers");
 module.exports = async (event, context, callback) => {
   try {
     const pujaList = await dbModels.Puja
-      .findAll();
+      .findAll({
+        include: [
+          {
+            model: dbModels.PujaLanguage,
+            attributes: ['LanguageId'],
+            required: true,
+            include: [
+              {
+                model: dbModels.Language,
+                attributes: ['name'],
+                required: true,
+                
+              }],
+          }
+        ]
+      });
     return helpers.success({ data: pujaList });
     callback(null, x)
   } catch (error) {
