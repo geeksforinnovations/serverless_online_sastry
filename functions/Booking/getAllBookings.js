cfceac0c9c2ae2ec191e5352d6ba41efc97a3319
Manjunath <manjunath.cycle@gmailc.om>
2020-01-18 15:57:53 +0530

    Added service for bookings API

--- functions/Booking/getAllBookings.js
+++ functions/Booking/getAllBookings.js
@@ -1,21 +1,6 @@
 const dbModels = require('../../models')
-const helpers = require("../../utils/helpers");
-
+const service = require('./booking.service')
 
 module.exports = async (event, context, callback) => {
-  try {
-    const bookings = await dbModels.Booking
-      .findAll({
-        include: [
-          {
-            model: dbModels.Puja,
-            required: true,
-            as: 'puja'
-          }
-        ]
-      });
-    return helpers.success({ data: bookings });
-  } catch (error) {
-    return helpers.failure({ message: error.message });
-  }
+    return service.getAllBookings();
 };
--- functions/Booking/updateBooking.js
+++ functions/Booking/updateBooking.js
@@ -1,27 +1,11 @@
-const dbModels = require('../../models')
-const helpers = require("../../utils/helpers");
+const service = require('./booking.service')
 
 module.exports = async (event, context, callback) => {
-    try {
         const booking = JSON.parse(event.body);;
-        //  var booking = {
-        //    id:2, pujaId: 1, languageId: 1, name: 'Manjunath test', phoneNumber: '123456',
-        //     bookingDate: new Date(), addressLine1: 'test lingampally', addressLine2: 'MIG test',
+        // var booking = {
+        //    id:2, pujaId: 1, languageId: 1, name: 'Test Manjunath', phoneNumber: '123456',
+        //     bookingDate: new Date(), addressLine1: 'lingampally', addressLine2: 'MIG',
         //     requirePujaType:'Online',videoCallUserName:'test@skype',status:'Active'
         // }; 
-        const bookingDetails = {
-            pujaId: booking.pujaId, languageId: booking.languageId, name: booking.name,
-            phoneNumber: booking.phoneNumber, bookingDate: booking.bookingDate,
-            addressLine1: booking.addressLine1, addressLine2: booking.addressLine2,
-            requirePujaType: booking.requirePujaType, videoCallUserName: booking.videoCallUserName,
-            status: booking.status
-        }
-        const updatedBooking = await dbModels.Booking
-            .update(bookingDetails, { where: { id: booking.id } });
-
-
-        return helpers.success({ data: updatedBooking });
-    } catch (error) {
-        return helpers.failure({ message: error.message });
-    }
+      return  service.updateBooking(booking);
 };
