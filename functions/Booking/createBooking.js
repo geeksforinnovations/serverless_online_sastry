0a3dae8fda7a864910dd5fba10f597b0f7c1a954
Mani Kumar K V <mkanamarlapudi@nasinsurance.com>
2020-01-27 15:50:39 +0530

    removed un used codein create booking

--- functions/Booking/createBooking.js
+++ functions/Booking/createBooking.js
@@ -7,11 +7,11 @@ const otpService = require('../otp/otp.service')
 module.exports = async (event, context, callback) => {
     try {
         const booking = JSON.parse(event.body);
-        const otpResult = await otpService.verifyOTP(booking.phoneNumber, booking.otpCode);
-        if (helpers.isUndefined(otpResult)) {
-            console.error('unable to send OTP')
-            throw new Error("otp failed")
-        }
+        // const otpResult = await otpService.verifyOTP(booking.phoneNumber, booking.otpCode);
+        // if (helpers.isUndefined(otpResult)) {
+        //     console.error('unable to send OTP')
+        //     throw new Error("otp failed")
+        // }
         const data = await service.createBooking(booking);
         emailService.sendBookingConfirmation(booking).then(respn => { })
         return helpers.success({ data: data });
